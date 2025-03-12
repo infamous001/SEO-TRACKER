fce874d1152097886a6a54b4ffb7eb4371239730
infamous001 <jindalrohit145@gmail.com>
2025-03-12 11:12:24 +0530

    add-domain-button-final

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <form action="" className="flex gap-2">
         <input className="bg-white border border-b-4 border-blue-200 px-4 py-2 text-l rounded-xl grow" type="text" placeholder="NewDomain.com"/>
-        <button className="bg-indigo-500 text-white px-8 rounded-xl">Add</button>
+        <button className="bg-indigo-500 text-white px-8 rounded-xl border border-b-4 border-indigo-700">Add</button>
       </form>
     </div>
   );
