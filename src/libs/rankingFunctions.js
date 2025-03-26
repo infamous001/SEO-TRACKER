28e379b9db8ab5e450bfc6d2897a3c4f1facfb45
infamous001 <jindalrohit145@gmail.com>
2025-03-26 10:00:10 +0530

    cronjob script

--- src/libs/rankingFunctions.js
+++ src/libs/rankingFunctions.js
@@ -13,5 +13,12 @@ export async function doGoogleSearch(keyword) {
   };
   
   const response=await axios.post(url, data, { headers });
-  return response.headers.get('x-response-id');
+  if (!response?.headers) {
+    console.error('no header in response '+url);
+    console.error(data);
+    return null;
+  } else {
+    console.log('responseId:'+response?.headers.get('x-response-id'));
+    return response?.headers.get('x-response-id');
+  }
 }
